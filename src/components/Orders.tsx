c7d28c5ca877d247df80916461f7eb455efeaa00
brdlysmth <brsmith110@gmail.com>
2020-09-25 14:23:07 -0400

    successfully pulled all orders in

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { Layout, Menu, Breadcrumb } from "antd";
+import { useQuery, gql } from "@apollo/client";
 const { Header, Content, Footer, Sider } = Layout;
 
+const ALL_ORDERS = gql`
+  query {
+    allOrders {
+      id
+    }
+  }
+`;
+
 const Orders: React.FC = () => {
-  return <Content> Orders </Content>;
+  const { loading, error, data } = useQuery(ALL_ORDERS);
+
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.allOrders.map((order: any) => {
+    return <Content> {order.id}</Content>;
+  });
 };
 
 export default Orders;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql/external",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        allOrders {
+          id
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
